c08b29046bebe91f94c376b42dd4d99932b3e012
Killerbee663 <ttawela@gmail.com>
2025-11-26 02:44:03 +0200

    fix database URL for psycopg3

--- config.py
+++ config.py
@@ -2,15 +2,23 @@ import os
 from dotenv import load_dotenv
 
 basedir = os.path.abspath(os.path.dirname(__file__))
-load_dotenv(os.path.join(basedir, '.env'))
+load_dotenv(os.path.join(basedir, ".env"))
 
 
 class Config:
     SECRET_KEY = os.environ.get("SECRET_KEY") or "you-will-never-guess"
 
-    SQLALCHEMY_DATABASE_URI = os.environ.get(
+    database_url = os.environ.get("DATABASE_URL")
+    if database_url and database_url.startswith("postgresql://"):
+        database_url = database_url.replace("postgresql://", "postgresql+psycopg://", 1)
+
+    SQLALCHEMY_DATABASE_URI = database_url or "sqlite:///" + os.path.join(
+        basedir, "app.db"
+    )
+
+    """SQLALCHEMY_DATABASE_URI = os.environ.get(
         "DATABASE_URL"
-    ) or "sqlite:///" + os.path.join(basedir, "app.db")
+    ) or "sqlite:///" + os.path.join(basedir, "app.db")"""
 
     MAIL_SERVER = os.environ.get("MAIL_SERVER")
     MAIL_PORT = int(os.environ.get("MAIL_PORT") or 25)
@@ -21,8 +29,8 @@ class Config:
 
     POSTS_PER_PAGE = 7
 
-    LANGUAGES = ['en', 'es', 'ro']
+    LANGUAGES = ["en", "es", "ro"]
 
-    GOOGLE_APPLICATION_CREDENTIALS = os.environ.get('GOOGLE_APPLICATION_CREDENTIALS')
+    GOOGLE_APPLICATION_CREDENTIALS = os.environ.get("GOOGLE_APPLICATION_CREDENTIALS")
 
-    ELASTICSEARCH_URL = os.environ.get('ELASTICSEARCH_URL')
+    ELASTICSEARCH_URL = os.environ.get("ELASTICSEARCH_URL")
